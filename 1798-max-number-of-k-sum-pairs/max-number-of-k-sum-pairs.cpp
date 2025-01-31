aa97f3eec736e09649368377fbdda7d7d86343cd
VINCENZ0 <159175299+BASAVARAJ-CHIPPALAKATTI@users.noreply.github.com>
2025-01-31 16:36:07 +0530

    Time: 88 ms (72.49%) | Memory: 62 MB (69.58%) - LeetSync

---
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -0,0 +1,24 @@
+// approach sorting and two pointers
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        int i=0;
+        int j=nums.size()-1;
+        sort(nums.begin(),nums.end());
+        int countPairs=0;
+        while(i<j){
+            if(nums[i]+nums[j]==k){
+                countPairs++;
+                i++;
+                j--;
+            }
+            else if(nums[i]+nums[j]< k){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        return countPairs;
+    }
+};
