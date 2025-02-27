e73f13029af8dcee415776788cd5dfeb32413ce3
VINCENZ0 <159175299+BASAVARAJ-CHIPPALAKATTI@users.noreply.github.com>
2025-02-27 19:28:35 +0530

    Time: 427 ms (11.96%) | Memory: 150 MB (21.37%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.cpp
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.cpp
@@ -1,6 +1,9 @@
 /*
 think a+b=c
 c-b=a
+
+two for loop for  c, b
+and recursion for finding sub fib
 */
 class Solution {
 public:
@@ -10,10 +13,11 @@ public:
         for(int i=0;i<n;i++){
             mp[arr[i]]=i;
         }
+        vector<vector<int>>t(n,vector<int>(n,-1));
         int maxlen=0;
         for(int j=0;j<n;j++){
             for(int k=j+1;k<n;k++){
-                int len=solve(j,k,arr,mp);
+                int len=solve(j,k,arr,mp ,t);
                 if(len>=3){
                     maxlen=max(maxlen,len);
                 }
@@ -21,11 +25,12 @@ public:
         }
         return maxlen;
     }
-    int solve(int j,int k,vector<int>&arr,unordered_map<int,int>& mp){
+    int solve(int j,int k,vector<int>&arr,unordered_map<int,int>& mp,vector<vector<int>>&t){
+        if(t[j][k]!=-1) return t[j][k];
         int target=arr[k]-arr[j];
         if(mp.count(target) && mp[target]<j){
             int i=mp[target];
-            return solve(i,j,arr,mp)+1; //plus one for k
+            return t[j][k]=solve(i,j,arr,mp,t)+1; //plus one for k
         }
         //if not match in map
         return 2; //plus two for i and j 
