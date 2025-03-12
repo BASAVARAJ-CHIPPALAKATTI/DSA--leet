e4ad942734df20c29776fabf9b9b7251dfba6263
VINCENZ0 <159175299+BASAVARAJ-CHIPPALAKATTI@users.noreply.github.com>
2025-03-12 19:34:34 +0530

    Time: 0 ms (100.00%) | Memory: 21.5 MB (10.71%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
-    int maximumCount(vector<int>& nums) {
-        auto countitp=[](int num){
-            return num>0;
-        };
-        auto countitn=[](int num){
-            return num<0;
-        };
-        int postive=count_if(nums.begin(),nums.end(),countitp);
-        int negtive=count_if(nums.begin(),nums.end(),countitn);
 
-        return max(postive,negtive);
+    int maximumCount(vector<int>& nums) {
+
+        // // using STL count_it  O(n)
+
+        // auto countitp=[](int num){
+        //     return num>0;
+        // };
+        // auto countitn=[](int num){
+        //     return num<0;
+        // };
+        // int postive=count_if(nums.begin(),nums.end(),countitp);
+        // int negtive=count_if(nums.begin(),nums.end(),countitn);
+
+        // return max(postive,negtive);
+
+        // using lower bound O(log(n))
+        int n=nums.size();
+        int Pidx=lower_bound(nums.begin(),nums.end(),1)-nums.begin(); // passing 1 checks >=1 
+        // -nums.begin() because return idx otherwise it returns number
+        int Nidx=lower_bound(nums.begin(),nums.end(),0)-nums.begin();
+
+        return max(n-Pidx,Nidx);
     }
 };
