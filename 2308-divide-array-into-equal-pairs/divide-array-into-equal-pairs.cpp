5e552b26b9b1d734010b43d7bee642008b950c20
VINCENZ0 <159175299+BASAVARAJ-CHIPPALAKATTI@users.noreply.github.com>
2025-03-17 12:25:28 +0530

    Time: 12 ms (5.86%) | Memory: 18.2 MB (31.79%) - LeetSync

---
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_set<int>st;
+        for(int &num : nums){
+            if(st.find(num)!=st.end()){
+                st.erase(num);
+            }
+            else{
+                st.insert(num);
+            }
+        }
+        return st.size()==0;
+    }
+};
