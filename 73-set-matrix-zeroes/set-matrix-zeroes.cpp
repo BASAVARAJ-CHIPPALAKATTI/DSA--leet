8d5d7a61a677656bf0122427b1efd9e87c2c1492
VINCENZ0 <159175299+BASAVARAJ-CHIPPALAKATTI@users.noreply.github.com>
2025-05-21 21:37:49 +0530

    Time: 3 ms (11.48%) | Memory: 18.8 MB (24.60%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,8 +1,11 @@
+//own
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         vector<int>row(matrix.size(),-1);   
         vector<int>col(matrix[0].size(),-1);
+        // aslo we can use set here
+        // to store i and j in set
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
                 if(matrix[i][j]==0){
