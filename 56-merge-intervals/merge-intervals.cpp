e7e7179b4d937391f73d9cac3a2dcb8ef4fa3394
VINCENZ0 <159175299+BASAVARAJ-CHIPPALAKATTI@users.noreply.github.com>
2025-05-24 00:13:48 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (59.49%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -4,9 +4,12 @@ public:
         sort(intervals.begin(),intervals.end());
         vector<vector<int>>res;
         res.push_back(intervals[0]);
-        for(int i=0;i<intervals.size();i++){
-            if(res.back()[1]>=intervals[i][0]){ //ovarlap
-                res.back()[1]=max(res.back()[1] ,intervals[i][1]);
+        for(int i=1;i<intervals.size();i++){
+            if(res.back()[1]>=intervals[i][0]){
+                if(res.back()[1]>=intervals[i][1]){
+                    continue;
+                }
+                res.back()[1]=intervals[i][1];
             }
             else{
                 res.push_back(intervals[i]);
