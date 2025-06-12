444e5efdda9a0a8dd35ae498266d1c4c41f19d7c
VINCENZ0 <159175299+BASAVARAJ-CHIPPALAKATTI@users.noreply.github.com>
2025-06-12 21:30:55 +0530

    Time: 4 ms (53.55%) | Memory: 25.9 MB (13.04%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -6,7 +6,7 @@ public:
         //else return true
 
         //for row
-        for(int row=0;row<9;row++){
+        /*for(int row=0;row<9;row++){
             unordered_set<char>st;
             for(int col=0;col<9;col++){
                 char val=board[row][col];
@@ -55,6 +55,30 @@ public:
                     return false;
                 }
                 st.insert(val);
+            }
+        }
+        return true;
+        */
+
+        //string(n, char) is a C++ constructor that creates a string consisting of n copies of the character char
+        //2nd approach
+        unordered_set<string>st;
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+                    continue;
+                }
+ //string(n, char) is a C++ constructor that creates a string consisting of n copies of the character char
+                    string row=string(1,board[i][j]) + "_row_" +to_string(i);  //for row
+                    string col=string(1,board[i][j]) + "_col_" +to_string(j);  //for col
+                    string box=string(1,board[i][j]) + "_box_" +to_string(i/3)+to_string(j/3); //for box
+                    if(st.count(row)|| st.count(col) || st.count(box)){
+                        return false;
+                    }
+                    st.insert(row);
+                    st.insert(col);
+                    st.insert(box);
+                
             }
         }
         return true;
