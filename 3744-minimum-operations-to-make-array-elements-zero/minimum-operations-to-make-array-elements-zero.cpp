36b6b69dd1456c964b7903f764a69e52ece01c30
VINCENZ0 <159175299+BASAVARAJ-CHIPPALAKATTI@users.noreply.github.com>
2025-09-06 19:52:06 +0530

    Time: 130 ms (15.94%) | Memory: 207.3 MB (15.94%) - LeetSync

--- 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.cpp
+++ 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
     long long minOperations(vector<vector<int>>& queries) {
-        vector<long long> lefts, rights, steps;
-        long long p = 1;
-        int k = 1;
-        while (p <= 1e9) {
-            long long L = p;
-            long long R = min((long long)1e9, 4 * p - 1);
-            lefts.push_back(L);
-            rights.push_back(R);
-            steps.push_back(k);
-            p *= 4;
-            k++;
+        long long ans=0;
+        for(auto q: queries){
+            int l=q[0];
+            int r=q[1];
+            long long steps=solve(l,r);
+            ans+=(steps+1)/2;  // in pair we need to do floor operation 
         }
-
-        long long ans = 0;
-
-        for (auto &q : queries) {
-            long long l = q[0], r = q[1];
-            long long totalSteps = 0;
-
-            for (int i = 0; i < (int)lefts.size(); i++) {
-                if (r < lefts[i] || l > rights[i]) continue; // no overlap
-                long long L = max(l, lefts[i]);
-                long long R = min(r, rights[i]);
-                long long len = R - L + 1;
-                totalSteps += len * steps[i];
-            }
-
-            // ceil(totalSteps / 2)
-            ans += (totalSteps + 1) / 2;
-        }
-
         return ans;
+    }
+    long long solve(int l,int r){
+        long long L=1;
+        long long S=1;
+        long long step=0;
+        while(L<=r){
+            long long R=L*4-1;
+            long long start=max(L,(long long)l);
+            long long end=min(R,(long long)r);
+            if(start <= end){
+                step+=(end-start+1)*S;
+            }
+            S++;
+            L=L*4;
+        }
+        return step;
     }
 };
