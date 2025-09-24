5060e287e1dffadde7d52cd2ac2b241d5599716a
VINCENZ0 <159175299+BASAVARAJ-CHIPPALAKATTI@users.noreply.github.com>
2025-09-24 23:02:14 +0530

    Time: 3 ms (40.40%) | Memory: 115.6 MB (85.29%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -2,11 +2,16 @@ class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
         int first=INT_MAX;
-        int second=INT_MAX;
-        for(int &num : nums){
-            if(num<=first) first=num;
-            else if(num<=second) second=num;
-            else{
+        int sec=INT_MAX;
+
+        for(int num : nums){
+            if(num <= first){
+                first=num;
+            }
+            else if(num <= sec){
+                sec=num;
+            }
+            else{  // num is greater than first and sec
                 return true;
             }
         }
