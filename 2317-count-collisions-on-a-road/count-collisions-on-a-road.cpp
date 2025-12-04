461c9520f5fcf530e181879284264f1e060b4f6b
VINCENZ0 <159175299+BASAVARAJ-CHIPPALAKATTI@users.noreply.github.com>
2025-12-04 22:52:17 +0530

    Time: 11 ms (65.80%) | Memory: 19.1 MB (84.64%) - LeetSync

---
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countCollisions(string directions) {
+        // just remove L from starting and R from last beacuse they no collide only
+        int n=directions.size();
+        int i=0;
+        int j=directions.size()-1;
+        while( i < n && directions[i]=='L'){
+            i++;
+        }
+        while(j>=0 && directions[j]=='R'){
+            j--;
+        }
+
+        int collision=0;
+        while(i <= j){
+            if(directions[i]=='L' || directions[i]== 'R'){
+                collision++;
+            }
+            i++;
+        }
+        return collision;
+    }
+};
