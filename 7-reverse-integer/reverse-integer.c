3072bcebb7a0d63df8d7f52a9f9249a6082a37e9
VINCENZ0 <159175299+BASAVARAJ-CHIPPALAKATTI@users.noreply.github.com>
2025-12-05 17:59:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (32.04%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.c
@@ -0,0 +1,11 @@
+int reverse(int x){
+   long int rev=0;
+    while( x){
+        rev = rev*10 + (x % 10);
+        x/=10;
+    }
+    if(rev < INT_MIN || rev >INT_MAX){
+        return 0;
+    }
+    return rev;
+}
