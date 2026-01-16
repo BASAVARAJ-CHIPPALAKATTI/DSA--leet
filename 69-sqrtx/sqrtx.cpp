5db51d576a75929889e5d558adecc315ef295ddb
VINCENZ0 <159175299+BASAVARAJ-CHIPPALAKATTI@users.noreply.github.com>
2026-01-16 23:47:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (84.47%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -7,14 +7,15 @@ public:
         while(low <= high){
             long long mid =low+ (high - low)/2;
     
-            if(mid * mid >=x){
+            if(mid * mid <=x){
                 ans =mid;
-                high =mid-1;
+                low =mid+1;
+                
             }
             else{
-                low =mid+1;
+                high =mid-1;
             }
         }
-        return (ans*ans==x) ? ans : ans-1;
+        return ans;
     }
 };
