20247053163c7277ebb3e1b8942a34efc9aa1191
VINCENZ0 <159175299+BASAVARAJ-CHIPPALAKATTI@users.noreply.github.com>
2026-02-02 19:50:42 +0530

    Time: 15 ms (28.60%) | Memory: 18.6 MB (14.72%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,31 +1,28 @@
-//revision
 class Solution {
 public:
-    vector<int> t=vector<int>(301,-1);
-    unordered_set<string>st;
-    int n;
+   int t[301];
     bool wordBreak(string s, vector<string>& wordDict) {
-        n=s.size();
-        for( string word : wordDict){
+        unordered_set<string>st;
+        for(string word : wordDict){
             st.insert(word);
         }
-        return solve(s,0);
+        memset(t,-1,sizeof(t));
+        return solve(s,st,0);
     }
-    bool solve(string s,int idx){
-        if(idx==n){
-            return true; // s is present in wordDict
-        }
-        if(st.count(s.substr(idx,n-idx))){
+    bool solve(string s, unordered_set<string>&st ,int i){
+        if(i==s.size()){
             return true;
         }
-        if(t[idx]!=-1){
-            return t[idx];
+        if(t[i]!=-1){
+            return t[i];
         }
-        for(int i=1;i<=n;i++){
-            if(st.count(s.substr(idx,i)) && solve(s,idx+i)){
-                return t[idx]= true;
+        for(int j=i;j<s.size();j++){
+            if(st.count(s.substr(i,j-i+1))){
+                if(solve(s,st,j + 1)) {
+                    return t[i] =true;
+                }
             }
         }
-        return t[idx] =false;
+        return t[i]=false;
     }
 };
