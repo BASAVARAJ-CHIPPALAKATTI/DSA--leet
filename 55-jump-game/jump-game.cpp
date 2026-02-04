7bccbb9324cefca82dfdce15a443c9c5d55fcb97
VINCENZ0 <159175299+BASAVARAJ-CHIPPALAKATTI@users.noreply.github.com>
2026-02-04 21:17:17 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (42.80%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,13 +1,18 @@
+// traverse= reverse loop from 2nd last elt
+// make it last one as var=lastreachable elt
+// evey time do that elemet is lastreachable
+// it it is reachable than change var to it's index and check that any elt recachable this lastreachable
+// it will do again and again if we get the 0 it's true
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n=nums.size();
-        int reachable=nums.size()-1;
+        int dest=n-1;
         for(int i=n-2;i>=0;i--){
-            if(nums[i]+i >= reachable){
-                reachable=i;
+            if(nums[i]+i >= dest){
+                dest=i;
             }
         }
-        return reachable==0;
+        return (dest==0);
     }
 };
