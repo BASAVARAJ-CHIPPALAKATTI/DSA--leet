b3adf444956b04e52b2a7867dce79e2a72eb5ad9
VINCENZ0 <159175299+BASAVARAJ-CHIPPALAKATTI@users.noreply.github.com>
2026-02-20 12:09:23 +0530

    Time: 7 ms (24.12%) | Memory: 26.9 MB (19.01%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
@@ -1,28 +1,27 @@
-// go for hint
 class Solution {
 public:
     int countServers(vector<vector<int>>& grid) {
-       vector<int>row(grid.size(),0);
-       vector<int>col(grid[0].size(),0);
-       int count=0;
-       for(int i=0;i<grid.size();i++){
+        int ans=0;
+        vector<int>row(grid.size(),0);
+        vector<int>col(grid[0].size(),0);
+        for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]==1){
                     row[i]++;
                     col[j]++;
                 }
             }
-       }
-
+        }
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]==1){
-                    if(row[i]>1 || col[j]>1){
-                        count++;
+                 if(grid[i][j]==1){
+                    if(row[i] > 1 || col[j] >1 ){
+                    ans++;
                     }
                 }
             }
         }
-        return count;
+        
+        return ans;
     }
 };
