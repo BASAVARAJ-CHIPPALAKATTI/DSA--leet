476e078e974d575c9dd186597787f4a74a8eb745
VINCENZ0 <159175299+BASAVARAJ-CHIPPALAKATTI@users.noreply.github.com>
2026-03-11 22:38:50 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (51.57%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int t[101][101];
     int uniquePaths(int m, int n) {
-        memset(t,-1,sizeof(t));
-        return solve(0,0,m,n);
-    }
-    int solve(int i,int j,int m,int n){
-        if(i>=m || j>=n){
-            return 0;
+        vector<vector<int>>grid(m,vector<int>(n,0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0){
+                    grid[i][j]=1;
+                }
+                else{
+                    if(i>0){
+                        grid[i][j]+=grid[i-1][j];
+                    }
+                    if(j>0){
+                        grid[i][j]+=grid[i][j-1];
+                    }
+                }
+            }
         }
-        if(i==m-1 && j==n-1){
-            return 1;
-        }
-        if(t[i][j]!=-1){
-            return t[i][j];
-        }
-        int right= solve(i,j+1,m,n);
-        int down=solve(i+1,j,m,n);
-        return t[i][j]= right+down;
+        return grid[m-1][n-1];
     }
 };
