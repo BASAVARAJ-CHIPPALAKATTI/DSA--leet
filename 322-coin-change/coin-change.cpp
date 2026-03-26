1570724b0971a1a2198f53e4aef07e863c32dadb
VINCENZ0 <159175299+BASAVARAJ-CHIPPALAKATTI@users.noreply.github.com>
2026-03-26 23:51:53 +0530

    Time: 35 ms (50.79%) | Memory: 13.7 MB (96.80%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,31 +1,30 @@
 class Solution {
-public: 
-    int dp[10001];
+public:
+    int t[13][10001];
     int coinChange(vector<int>& coins, int amount) {
-        memset(dp,-1,sizeof(dp));
-        int ans = solve(coins,amount);
+        memset(t,-1,sizeof(t));
+        int ans=solve(coins,amount,0);
         if(ans==INT_MAX){
             return -1;
         }
         return ans;
     }
-    int solve(vector<int>& coins, int amount){
-        if(amount ==0){
+    int solve(vector<int>&coins,int amount,int i){
+        if(amount==0){
             return 0;
         }
-        if(amount < 0){
+        if(i>=coins.size() || amount <0){
             return INT_MAX;
         }
-        if(dp[amount]!=-1){
-            return dp[amount];
+        if(t[i][amount]!=-1){
+            return t[i][amount];
         }
-        int ans=INT_MAX;
-        for(int coin : coins){
-            int res=solve(coins,amount-coin);
-            if(res!=INT_MAX){
-                ans=min(ans,1+res);
-            }
+        int pick=solve(coins,amount-coins[i],i);
+        if(pick!=INT_MAX){
+            pick+=1;
         }
-        return dp[amount] = ans;
+        int notpick=solve(coins,amount,i+1);
+
+        return t[i][amount] =min(pick,notpick);
     }
 };
