da73012cb60bdbac3927ae8103e798c222dce775
VINCENZ0 <159175299+BASAVARAJ-CHIPPALAKATTI@users.noreply.github.com>
2026-03-29 16:42:28 +0530

    Time: 3 ms (28.78%) | Memory: 15.8 MB (6.55%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -6,12 +6,11 @@ public:
         solve(nums,0,currans);
         return ans;
     }
-    void solve(vector<int>nums, int i,vector<int>currans){
+    void solve(vector<int>& nums,int i,vector<int>currans){
         if(i==nums.size()){
             ans.push_back(currans);
-            return;
+            return ;
         }
-        //for take
         currans.push_back(nums[i]);
         solve(nums,i+1,currans);
         currans.pop_back();
