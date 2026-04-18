04f16049e97ade68d7c83258a1ea2d43e5b8a012
VINCENZ0 <159175299+BASAVARAJ-CHIPPALAKATTI@users.noreply.github.com>
2026-04-18 22:43:41 +0530

    Time: 3 ms (44.43%) | Memory: 39 MB (99.84%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
@@ -1,25 +1,23 @@
+//easy
 class Solution {
 public:
     long long maxMatrixSum(vector<vector<int>>& matrix) {
         long long ans=0;
+        int total_min=0;
         int minval=INT_MAX;
-        int count=0;
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
-                if(matrix[i][j]<0){
-                    count++;
-                    ans+=matrix[i][j]*-1;
-                    minval=min(minval,matrix[i][j]*-1);
+                if(matrix[i][j]<0 ){
+                    total_min=(total_min+1) % 2;
                 }
-                else{
-                    ans+=matrix[i][j];
-                    minval=min(minval,matrix[i][j]);
-                }  
+                int val=abs(matrix[i][j]);
+                ans+=val;
+                minval=min(minval,val);
             }
         }
-        if(count%2==0){
+        if(total_min % 2==0){
             return ans;
         }
-        return ans-minval*2;
+        return ans-(minval*2);
     }
 };
