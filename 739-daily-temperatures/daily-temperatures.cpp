99dce2b7ca856b38ada4b2a24e198c7b57cb186e
VINCENZ0 <159175299+BASAVARAJ-CHIPPALAKATTI@users.noreply.github.com>
2026-05-14 00:57:59 +0530

    Time: 24 ms (46.31%) | Memory: 107.4 MB (20.40%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -1,13 +1,13 @@
-// use stack and store the idx
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n=temperatures.size();
+        vector<int>ans(n,0);
         stack<int>st;
-        vector<int>ans(temperatures.size(),0);
-        for(int i=0;i<temperatures.size();i++){
-            while(!st.empty() && temperatures[st.top()]<temperatures[i]){
-                int idx=st.top();
-                ans[idx]=i-idx;
+
+        for(int i=0;i<n;i++){
+            while(!st.empty() && temperatures[st.top()] < temperatures[i]){
+                ans[st.top()] = i-st.top();
                 st.pop();
             }
             st.push(i);
