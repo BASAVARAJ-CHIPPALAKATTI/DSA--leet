b93650783e1e628b9fee10c99265e519b9709749
VINCENZ0 <159175299+BASAVARAJ-CHIPPALAKATTI@users.noreply.github.com>
2026-05-31 13:38:13 +0530

    Time: 41 ms (13.87%) | Memory: 106.8 MB (48.03%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(),asteroids.end());
+        long long curr=mass;
+        for(int i=0;i<asteroids.size();i++){
+            if(curr < asteroids[i]){
+                return false;
+            }
+            curr +=asteroids[i];
+        }
+        return true;
+    }
+
+};
