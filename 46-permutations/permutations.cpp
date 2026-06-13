14b6641992d81a4fdc253029fd2a2bbd5ed029d1
VINCENZ0 <159175299+BASAVARAJ-CHIPPALAKATTI@users.noreply.github.com>
2026-06-13 21:37:55 +0530

    Time: 12 ms (5.63%) | Memory: 15.6 MB (5.21%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
+    vector<vector<int>>ans;
     vector<vector<int>> permute(vector<int>& nums) {
+        unordered_set<int>st;
         vector<int>currans;
-        set<int>st;
-        solve(currans,st,nums);
+        solve(nums,currans,st);
         return ans;
     }
-    void solve(vector<int>&currans,set<int>&st,vector<int>&nums){
+    void solve(vector<int>& nums,vector<int>currans,unordered_set<int>st){
         if(currans.size()==nums.size()){
             ans.push_back(currans);
             return ;
         }
         for(int i=0;i<nums.size();i++){
-            if(st.count(nums[i])){
-                continue;
+            if(!st.count(nums[i])){
+                currans.push_back(nums[i]);
+                st.insert(nums[i]);
+                solve(nums,currans,st);
+                currans.pop_back();
+                st.erase(nums[i]);
             }
-            currans.push_back(nums[i]);
-            st.insert(nums[i]);
-            solve(currans,st,nums);
-            currans.pop_back();
-            st.erase(nums[i]);
         }
     }
 };
