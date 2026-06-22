395014c092249cbe1274febdded2d981ba99cc27
VINCENZ0 <159175299+BASAVARAJ-CHIPPALAKATTI@users.noreply.github.com>
2026-06-22 21:33:13 +0530

    Time: 13 ms (33.49%) | Memory: 43.4 MB (78.47%) - LeetSync

--- 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.cpp
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.cpp
@@ -1,36 +1,73 @@
 //easy tow approach
+//one is simple traverse
+//sec using map;
 class Solution {
 public:
-    vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
-        int m=grid.size();
-        //bottom left side 
-        //col is fix
-        for(int row=0;row<m;row++){
-            solve(row,0,grid,false);  //true means ascending
-        }
-        for(int col=1;col<m;col++){
-            solve(0,col,grid,true);
-        }
-        return grid;
-    }
-
-    void solve(int row,int col,vector<vector<int>>&grid,bool ascend){
-        vector<int>vec;
+    void solve(int row,int col,vector<vector<int>>& grid,bool ascend){
+        int n=grid.size();
         int i=row;
         int j=col;
-        while(row <grid.size() && col <grid.size()){
-            vec.push_back(grid[row][col]);
-            row++;
-            col++;
+        vector<int>vec;
+        while(i<n && j<n){
+            vec.push_back(grid[i][j]);
+            i++;
+            j++;
         }
         if(ascend){
             sort(vec.begin(),vec.end());
         }
         else{
-            sort(vec.rbegin(),vec.rend()); //decending
+            sort(vec.begin(),vec.end(),greater<int>());
         }
-        for(int a=0;a<vec.size();a++){
-            grid[i++][j++]=vec[a];
+        i=row;
+        j=col;
+        for(int val : vec){
+            grid[i++][j++]=val;
         }
     }
+    vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
+        int n=grid.size();
+        for(int row=0;row<n;row++){
+            solve(row,0,grid,false);
+        }
+        for(int col=1;col<n;col++){
+            solve(0,col,grid,true);
+        }
+        return grid;
+    }
+    
 };
+// class Solution {
+// public:
+//     vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
+//         int n = grid.size();
+
+//         unordered_map<int, vector<int>> mp;
+//         //(i-j) -> diagonal elements
+
+//         for(int i = 0; i < n; i++) {
+//             for(int j = 0; j < n; j++) {
+//                 int diag = i-j;
+//                 mp[diag].push_back(grid[i][j]);       
+//             }
+//         }
+
+//         for(auto &it : mp) {
+//             if(it.first >= 0) { //Key
+//                 sort(begin(it.second), end(it.second));
+//             } else {
+//                 sort(rbegin(it.second), rend(it.second));
+//             }
+//         }
+
+//         for(int i = 0; i < n; i++) {
+//             for(int j = 0; j < n; j++) {
+//                 int diag = i-j;
+//                 grid[i][j] = mp[diag].back();
+//                 mp[diag].pop_back();      
+//             }
+//         }
+
+//         return grid;
+//     }
+// };
